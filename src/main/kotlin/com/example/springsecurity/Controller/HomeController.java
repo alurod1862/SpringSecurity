d2ea8073896b19bbd2d4b55a09ad7da1a18151bc
alu10711862 <alurod1862@ieselcaminas.org>
2023-11-21 20:50:11 +0100

    21/11

---
+++ src/main/kotlin/com/example/springsecurity/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.springsecurity.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/","/home"})
+    public String home(@RequestParam(name = "nombre", required = true, defaultValue = "Iker") String nombre, Model model) {
+        model.addAttribute("nombre", nombre);
+        return "home";
+
+    }
+}
+
