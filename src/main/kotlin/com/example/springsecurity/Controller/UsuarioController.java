cc35393420609da5855e86b8099adb21212fc22a
alurod1862 <alurod1862@ieselcaminas.org>
2023-11-27 13:31:45 +0100

    27/11

--- src/main/kotlin/com/example/springsecurity/Controller/UsuarioController.java
+++ src/main/kotlin/com/example/springsecurity/Controller/UsuarioController.java
@@ -6,22 +6,19 @@ import com.example.springsecurity.Models.Usuario;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/app")
 public class UsuarioController {
+
     @GetMapping("perfil")
     public String perfil(Model model) {
         Usuario usuario = new Usuario();
-        //usuario.setNombre("Iker");
-        //usuario.setApellido("Rodriguez");
-        //model.addAttribute("usuario", usuario);
-        //model.addAttribute("titulo","Perfil del usuario".concat(usuario.getNombre()));
+        usuario.setNombre("Iker");
+        usuario.setApellido("Rodriguez");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario " + usuario.getNombre());
         return "perfil";
     }
 
-
-
-
-
 }
+
--- src/main/kotlin/com/example/springsecurity/Models/Usuario.java
+++ src/main/kotlin/com/example/springsecurity/Models/Usuario.java
@@ -9,7 +9,7 @@ public class Usuario {
         return nombre;
     }
 
-    public void setNombre(String Nombre) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
